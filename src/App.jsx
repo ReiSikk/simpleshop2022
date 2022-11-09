80d8fe097c4c9531d7516bb08850415d4a5f7827
Rei Sikk <reix0010@stud.kea.dk>
2022-11-09 16:38:13 +0100

    cart introduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-function App() {
-  const [count, setCount] = useState(0);
+import { useEffect, useState } from "react";
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
 
-  return <div className="App"></div>;
+function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  function addToCart(data) {
+    //do we have the product
+    if (cart.find((entry) => entry.id === data.id)) {
+      setCart((oldCart) =>
+        oldCart.map((entry) => {
+          if (entry.id !== data.id) {
+            return entry;
+          }
+          //make a copy of the entry
+          const copy = { ...entry };
+          //set the correct amount
+          copy.amount = copy.amount + 1;
+          return copy;
+        })
+      );
+    } else {
+      //add a property called amount if it's not in the state already
+      setCart((oldCart) => oldCart.concat({ ...data, amount: 1 }));
+    }
+  }
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("https://kea-alt-del.dk/t7/api/products");
+      const data = await res.json();
+      setProducts(data);
+    }
+    getData();
+  }, []);
+  return (
+    <div className="App">
+      <Header />
+      <ProductList products={products} addToCart={addToCart} />
+      <Basket products={products} cart={cart} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Basket.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Basket(props) {
+  return (
+    <section className="Basket">
+      <ul>
+        {props.cart.map((item) => (
+          <li>
+            {item.productdisplayname} x {item.amount}, {item.amount * item.price}
+          </li>
+        ))}
+        {/*  <li>SomeProduct x 3, 1234,-</li> */}
+      </ul>
+      <button>Buy now</button>
+    </section>
+  );
+}
+
+export default Basket;
