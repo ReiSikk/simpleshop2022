245d9c5e470f118bdd5342eb50835cdb722d6f54
Rei Sikk <reix0010@stud.kea.dk>
2022-11-11 12:11:56 +0100

    checkoutform done

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,32 +1,43 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { insertOrder } from "../modules/db";
 function CheckoutForm(props) {
   const theForm = useRef(null);
-  function submit(e) {
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
+
+  async function submit(e) {
     e.preventDefault();
-    insertOrder({
-      name: theForm.current.elements.name.value,
+    const response = await insertOrder({
+      name: theForm.current.elements.email.value,
       email: theForm.current.elements.email.value,
       address: theForm.current.elements.address.value,
       basket: props.cart,
     });
+    if (response && response.length) {
+      setPaymentCompleted(true);
+    }
   }
   return (
-    <form onSubmit={submit} ref={theForm}>
-      <div className="form-control">
-        <label htmlFor="form-name">Name</label>
-        <input required type="text" name="name" id="form-name" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-email">Email</label>
-        <input required type="email" name="email" id="form-email" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-address">Address</label>
-        <textarea required name="address" id="form-address" />
-      </div>
-      <button>Pay</button>
-    </form>
+    <>
+      {paymentCompleted ? (
+        <p>Thank You!</p>
+      ) : (
+        <form onSubmit={submit} ref={theForm}>
+          <div className="form-control">
+            <label htmlFor="form-name">Name</label>
+            <input required type="text" name="name" id="form-name" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-email">Email</label>
+            <input required type="email" name="email" id="form-email" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-address">Address</label>
+            <textarea required name="address" id="form-address" />
+          </div>
+          <button>Pay</button>
+        </form>
+      )}
+    </>
   );
 }
 //name
--- src/modules/db.js
+++ src/modules/db.js
@@ -1,35 +1,31 @@
-export function insertOrder(payload) {
+export async function insertOrder(payload) {
   const key =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6dnhic2FiYnR2eWphZGFyenp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDA0NTQsImV4cCI6MTk4MzY3NjQ1NH0.Z4BtwWfFmlNfVJk5UlDZ9OMPrfXOSQaCkfTY6zyUCNE";
   const url = "https://xzvxbsabbtvyjadarzzt.supabase.co";
-  fetch(url + "/rest/v1/simpleshop", {
+  const res = await fetch(url + "/rest/v1/simpleshop", {
     method: "POST",
     headers: {
       apikey:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6dnhic2FiYnR2eWphZGFyenp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDA0NTQsImV4cCI6MTk4MzY3NjQ1NH0.Z4BtwWfFmlNfVJk5UlDZ9OMPrfXOSQaCkfTY6zyUCNE",
       "Content-Type": "application/json",
-      Prefer: "return=presentation",
+      Prefer: "return=representation",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6dnhic2FiYnR2eWphZGFyenp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDA0NTQsImV4cCI6MTk4MzY3NjQ1NH0.Z4BtwWfFmlNfVJk5UlDZ9OMPrfXOSQaCkfTY6zyUCNE",
     },
     body: JSON.stringify(payload),
   });
-  console.log(payload);
+  return await res.json();
 }
 
 /* 
 
-const options = {
-  method: 'POST',
-  headers: {
-    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpY2Njanp4a2xscWxoYnVvenFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg3MjYsImV4cCI6MTk4MjE3NDcyNn0.0fy2lU1BbfgzXVllYJiYei1pl2Pua6RR9ndixISNEXU',
-    'Content-Type': 'application/json',
-    Prefer: 'return=presentation'
-  },
-  body: '{"name":"Game of Thrones","seasons":2,"director":"Gareth Davies","rating":4.15,"ongoing":true}'
+const options = {  method: 'POST',  headers: {    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6dnhic2FiYnR2eWphZGFyenp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDA0NTQsImV4cCI6MTk4MzY3NjQ1NH0.Z4BtwWfFmlNfVJk5UlDZ9OMPrfXOSQaCkfTY6zyUCNE',    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6dnhic2FiYnR2eWphZGFyenp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDA0NTQsImV4cCI6MTk4MzY3NjQ1NH0.Z4BtwWfFmlNfVJk5UlDZ9OMPrfXOSQaCkfTY6zyUCNE',    'Content-Type': 'application/json',    Prefer: 'return=representation'  },  body: '{"name":"Jonas","email":"a@a.dk","address":"secret"}'
 };
 
-fetch('https://kicccjzxkllqlhbuozqh.supabase.co/rest/v1/tvshows', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+fetch('https://xzvxbsabbtvyjadarzzt.supabase.co/rest/v1/simpleshop', options)  .then(response => response.json())  .then(response => console.log(response))  .catch(err => console.error(err));
+
+
+
+
 
 */
